250a92d6f355f1eee03738c5e2d7766bb4230525
Friso-klinge <friso.klinge@student.nhlstenden.com>
2026-02-06 10:33:26 +0100

    movement script with time

--- sketchBasicMovement/sketchBasicMovement.ino
+++ sketchBasicMovement/sketchBasicMovement.ino
@@ -2,25 +2,45 @@ const int PIN_1A = 10;
 const int PIN_2A = 9;
 const int PIN_1B = 6;
 const int PIN_2B = 5;
+const int PIN_1R = 13;
+const int PIN_2R = 12;
+
 void setup() {
-  // put your setup code here, to run once:
   pinMode(PIN_1A, OUTPUT);
   pinMode(PIN_2A, OUTPUT);
   pinMode(PIN_1B, OUTPUT);
   pinMode(PIN_2B, OUTPUT);
+  pinMode(PIN_2R, INPUT);
+  pinMode(PIN_2R, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  forward(20);
+  backward(20);
   
 }
 
-void forward(int angle){
+void forward(int distance){
   analogWrite(PIN_2A, 200);
   analogWrite(PIN_2B, 200);
+  while (distance > 0){
+    distance = distance - 1;
+    Serial.println(distance);
+    delay(100);
+  }
+  analogWrite(PIN_2A, 0);
+  analogWrite(PIN_2B, 0);
 }
 
-void backward(int angle){
+void backward(int distance){
   analogWrite(PIN_1A, 200);
   analogWrite(PIN_1B, 200);
+  while (distance > 0){
+    distance = distance - 1;
+    Serial.println(distance);
+    delay(100);
+  }
+  analogWrite(PIN_1A, 0);
+  analogWrite(PIN_1B, 0);
 }
