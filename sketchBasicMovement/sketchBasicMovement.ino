052dafe35a0fee95f9a5f5a6f2b0935410e58030
Friso-klinge <friso.klinge@student.nhlstenden.com>
2026-02-11 15:22:57 +0100

    basic movement forward and backwards based on pulses

--- sketchBasicMovement/sketchBasicMovement.ino
+++ sketchBasicMovement/sketchBasicMovement.ino
@@ -6,9 +6,15 @@ const int PIN_1R = 13;
 const int PIN_2R = 12;
 const int GRIPPER = 11;
 const byte WHEEL_SPEED = 255;
-int lastRead = LOW;
+int RightPulses = 0;
+int LastInterruptRight = 0;
+int LeftPulses = 0;
+int LastInterruptLeft = 0;
+int RightWheel;
+int LeftWheel;
 
-void setup() {
+void setup() 
+{
   pinMode(PIN_1A, OUTPUT);
   pinMode(PIN_2A, OUTPUT);
   pinMode(PIN_1B, OUTPUT);
@@ -16,63 +22,93 @@ void setup() {
   pinMode(PIN_1R, INPUT);
   pinMode(PIN_2R, INPUT);
   Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(2), countPulseRightWheel, RISING);
+  attachInterrupt(digitalPinToInterrupt(3), countPulseLeftWheel, RISING);
+
+  //Forward 20 is een rotatie
+  // elke rotatie is ~20 cm
+  drive(80, 80);
+  drive(-80, -80);
+//  drive(-10, 10);
+//  drive(10, -10);
+}
+  
+void loop() 
+{
+  
 }
 
-void loop() {
-  forward(40);
-  backward(40);
-  left(5);
-  right(5);
+void countPulseRightWheel()
+{
+  if(millis() - LastInterruptRight > 20)
+  {
+    RightPulses++;
+    LastInterruptRight = millis();
+  }
 }
 
-void forward(int distance){
-  analogWrite(PIN_2A, WHEEL_SPEED);
-  analogWrite(PIN_2B, WHEEL_SPEED);
-  while (distance > 0){
-    if (digitalRead(PIN_1R) == HIGH && lastRead == LOW){
-      distance = distance - 1;
-      Serial.println(distance);
+void countPulseLeftWheel()
+{
+  if(millis() - LastInterruptLeft > 20)
+  {
+    LeftPulses++;
+    LastInterruptLeft = millis();
+  }
+}
+
+void drive(int RightDistance, int LeftDistance)
+{  
+  if(RightDistance > 0)
+  {
+    RightWheel = PIN_2A;
+  } else {
+    RightWheel = PIN_1A;
+  }
+
+  if(LeftDistance > 0)
+  {
+    LeftWheel = PIN_2B;
+  } else {
+    LeftWheel = PIN_1B;
+  }
+  
+  while(RightPulses <= abs(RightDistance) || LeftPulses <= abs(LeftDistance)){
+    analogWrite(RightWheel, WHEEL_SPEED);
+    analogWrite(LeftWheel, WHEEL_SPEED);
+
+    if(LeftPulses > RightPulses)
+    {
+      analogWrite(RightWheel, WHEEL_SPEED / 1.5);
     }
-    lastRead = digitalRead(PIN_1R);
+    if(RightPulses > LeftPulses)
+    {
+      analogWrite(LeftWheel, WHEEL_SPEED / 1.5);
+    }
+    
+    if(RightPulses >= abs(RightDistance))
+    {
+      analogWrite(RightWheel, 0);
+    }
+    if(LeftPulses >= abs(LeftDistance))
+    {  
+      analogWrite(LeftWheel, 0);
+    } 
+    debug(LeftPulses, abs(LeftDistance), RightPulses, abs(RightDistance));
   }
   analogWrite(PIN_2A, 0);
   analogWrite(PIN_2B, 0);
-  delay(20);
+  RightPulses = 0;
+  LeftPulses = 0;
+  delay(500);
 }
 
-void backward(int distance){
-  analogWrite(PIN_1A, WHEEL_SPEED);
-  analogWrite(PIN_1B, WHEEL_SPEED);
-  while (distance > 0){
-    distance = distance - 1;
-    delay(100);
-  }
-  analogWrite(PIN_1A, 0);
-  analogWrite(PIN_1B, 0);
-  delay(20);
-}
-
-void left(int distance){
-  analogWrite(PIN_1A, WHEEL_SPEED);
-  analogWrite(PIN_2B, WHEEL_SPEED);
-  while (distance > 0){
-    distance = distance - 1;
-    delay(100);
-  }
-  analogWrite(PIN_1A, 0);
-  analogWrite(PIN_2B, 0);
-  delay(20);
-}
-
-
-void right(int distance){
-  analogWrite(PIN_2A, WHEEL_SPEED);
-  analogWrite(PIN_1B, WHEEL_SPEED);
-  while (distance > 0){
-    distance = distance - 1;
-    delay(100);
-  }
-  analogWrite(PIN_2A, 0);
-  analogWrite(PIN_1B, 0);
-  delay(20);
+void debug(int a, int b, int c, int d)
+{
+  Serial.print(a);
+  Serial.print("/");
+  Serial.print(b);
+  Serial.print(" --- ");
+  Serial.print(c);
+  Serial.print("/");
+  Serial.println(d);
 }
--- sketch_feb10a/sketch_feb10a.ino
+++ sketch_feb10a/sketch_feb10a.ino
@@ -1,23 +1,15 @@
-int pulses = 0;
+
 
 void setup() {
-  pinMode(9, OUTPUT);
-  pinMode(13, INPUT);
+  attachInterrupt(digitalPinToInterrupt(2), countPulse, RISING);
   Serial.begin(9600);
-  
-  // put your setup code here, to run once:
-  digitalWrite(9, 200);
-  while (millis() < 1000){
-    if (digitalRead(13) == LOW){
-      pulses++;
-      Serial.println(pulses);
-    }
-    Serial.print("-");
-  }
-  digitalWrite(9, 0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   
+
+}
+
+void countPulse(){
+  Serial.println(millis());
 }
