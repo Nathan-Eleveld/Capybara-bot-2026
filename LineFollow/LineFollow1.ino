322defd9f49744fa69f71ec761733d27d4e3152c
AtakanKaracaNHL <atakan.karaca@student.nhlstenden.com>
2026-03-04 14:19:51 +0100

    Refactor LineFollow1.ino for improved readability

--- LineFollow/LineFollow1.ino
+++ LineFollow/LineFollow1.ino
@@ -1,164 +1,126 @@
-const int RIGHT_BACKWARD = 10;
-const int RIGHT_FORWARD  = 9;
-const int LEFT_BACKWARD  = 6;
-const int LEFT_FORWARD   = 5;
-
-const int RIGHT_IN = 2;
-const int LEFT_IN  = 3;
-
-const byte WHEEL_SPEED = 255;
-
-const int SENSOR_PINS[8] = {A0, A1, A2, A3, A4, A5, A6, A7};
-int sensorReadings[8];
-
-volatile int rightPulses = 0;
-volatile int leftPulses  = 0;
-
-volatile unsigned long lastInterruptRight = 0;
-volatile unsigned long lastInterruptLeft  = 0;
+const int   RIGHT_BACKWARD = 10;
+const int   RIGHT_FORWARD = 9;
+const int   LEFT_BACKWARD = 6;
+const int   LEFT_FORWARD = 5;
+const int   RIGHT_IN = 2;
+const int   LEFT_IN = 3;
+const byte  WHEEL_SPEED = 255;
+const int   SENSOR_PINS[8] = {A0, A1, A2, A3, A4, A5, A6, A7};
+int   sensorReadings[8];
+int   rightPulses = 0;
+int   lastInterruptRight = 0;
+int   leftPulses = 0;
+int   lastInterruptLeft = 0;
+int   rightWheel;
+int   leftWheel;
+bool  turn;
 
 void setup() {
-  Serial.begin(9600); // start seriële communicatie
-
-  for (int i = 0; i < 8; i++) {
-    pinMode(SENSOR_PINS[i], INPUT); // lijnsensor pins als input
+  Serial.begin(9600);
+  for(int pin : SENSOR_PINS){
+    pinMode(pin, INPUT);    
   }
+  pinMode(RIGHT_BACKWARD, OUTPUT);
+  pinMode(RIGHT_FORWARD, OUTPUT);
+  pinMode(LEFT_BACKWARD, OUTPUT);
+  pinMode(LEFT_FORWARD, OUTPUT);
+  pinMode(RIGHT_IN, INPUT);
+  pinMode(LEFT_IN, INPUT);
 
-  pinMode(RIGHT_BACKWARD, OUTPUT); // motor rechts achteruit
-  pinMode(RIGHT_FORWARD, OUTPUT);  // motor rechts vooruit
-  pinMode(LEFT_BACKWARD, OUTPUT);  // motor links achteruit
-  pinMode(LEFT_FORWARD, OUTPUT);   // motor links vooruit
-
-  digitalWrite(RIGHT_BACKWARD, LOW); // motor uit bij start
-  digitalWrite(RIGHT_FORWARD, LOW);
-  digitalWrite(LEFT_BACKWARD, LOW);
-  digitalWrite(LEFT_FORWARD, LOW);
-
-  pinMode(RIGHT_IN, INPUT_PULLUP); // encoder rechts input
-  pinMode(LEFT_IN, INPUT_PULLUP);  // encoder links input
-
-  attachInterrupt(digitalPinToInterrupt(RIGHT_IN), countPulseRightWheelISR, RISING); // interrupt rechter encoder
-  attachInterrupt(digitalPinToInterrupt(LEFT_IN),  countPulseLeftWheelISR,  RISING); // interrupt linker encoder
+  attachInterrupt(digitalPinToInterrupt(RIGHT_IN), countPulserightWheelISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(LEFT_IN), countPulseleftWheelISR, RISING);
 }
 
 void loop() {
-  getReadings(); // lees sensoren
-  drive();       // stuur motoren
+  getReadings(); 
+  drive();
 }
 
-// telt pulsen van de rechter encoder
-// debounce van 20ms voorkomt dubbele pulsen door ruis
-void countPulseRightWheelISR() {
-  unsigned long now = millis();
-  if (now >= lastInterruptRight) {
+//Function called when right wheel rotates 1/20th of a rotation
+void countPulserightWheelISR(){
+  if(millis() > lastInterruptRight){
     rightPulses++;
-    lastInterruptRight = now + 20;
+    lastInterruptRight = millis() + 20;
   }
 }
 
-// telt pulsen van de linker encoder
-// debounce van 20ms voorkomt dubbele pulsen door ruis
-void countPulseLeftWheelISR() {
-  unsigned long now = millis();
-  if (now >= lastInterruptLeft) {
+//Function called when left wheel rotates 1/20th of a rotation
+void countPulseleftWheelISR(){
+  if(millis() > lastInterruptLeft){
     leftPulses++;
-    lastInterruptLeft = now + 20;
+    lastInterruptLeft = millis() + 20;
   }
 }
 
-// leest alle sensoren en zet waarde naar 0 of 1
-// waarde onder 700 wordt gezien als lijn
-void getReadings() {
-  for (int i = 0; i < 8; i++) {
-    int raw = analogRead(SENSOR_PINS[i]); // lees analoge sensorwaarde
-    sensorReadings[i] = raw < 700;        // 1 = lijn, 0 = geen lijn
+void getReadings(){
+  int i = 0;
+  for(int pin : SENSOR_PINS){
+  RightWheel
+    sensorReadings[i] = (analogRead(pin) < 700);
+    //will be calibrated per sensor later
+    i++;
   }
 }
 
-// bepaalt motorsnelheid op basis van sensorpositie
-// als geen sensor lijn ziet wordt achteruit gereden
-void drive() {
-
-  stopMotors();
-
-  switch (getAverageSensorPin()) {
-
+void drive(){
+    analogWrite(RIGHT_FORWARD, 0);
+    analogWrite(LEFT_FORWARD, 0);
+    analogWrite(RIGHT_BACKWARD, 0);
+    analogWrite(LEFT_BACKWARD, 0);
+  
+  switch(getAverageSensorPin()){
     case 1:
       writeWheels(1, 0);
       break;
-
     case 2:
       writeWheels(1, 0.5);
       break;
-
     case 3:
       writeWheels(1, 0.75);
       break;
-
     case 4:
       writeWheels(1, 1);
       break;
-
     case 5:
       writeWheels(0.75, 1);
       break;
-
     case 6:
       writeWheels(0.5, 1);
       break;
-
     case 7:
       writeWheels(0, 1);
       break;
-
     case 0:
       analogWrite(RIGHT_BACKWARD, WHEEL_SPEED);
-      analogWrite(LEFT_BACKWARD,  WHEEL_SPEED);
+      analogWrite(LEFT_BACKWARD, WHEEL_SPEED);
       break;
   }
 }
 
-// berekent gemiddelde sensorpositie van alle sensoren die de lijn zien
-// als geen sensor actief is wordt 0 teruggegeven
-int getAverageSensorPin() {
-
-  float sum = 0.0;
-  int count = 0;
-
-  for (int i = 0; i < 8; i++) {
-    if (sensorReadings[i]) {   // gebruik sensoren die de lijn zien
-      sum += i + 1;            // 1..8 positie
-      count++;
+int getAverageSensorPin(){
+  float medianReading = 0.0;
+  int readingCount = 0;
+  for(int i = 0; i < 8; i++){
+    if(!sensorReadings[i]){
+      medianReading += i+ 1;
+      readingCount++;
     }
-  }
-
-  if (count == 0) {
-    return 0;
-  }
-
-  return (int)(sum / count + 0.5); // afronden voor stabieler sturen
+  }  
+  return medianReading / readingCount;
 }
 
-// schrijft motorsnelheid vooruit met multiplier voor sturen
-void writeWheels(float multiplierRight, float multiplierLeft) {
-  analogWrite(RIGHT_FORWARD, (int)(WHEEL_SPEED * multiplierRight));
-  analogWrite(LEFT_FORWARD,  (int)(WHEEL_SPEED * multiplierLeft));
+void writeWheels(float multiplierRight, float multiplierLeft){
+  analogWrite(RIGHT_FORWARD, WHEEL_SPEED * multiplierRight);
+  analogWrite(LEFT_FORWARD, WHEEL_SPEED * multiplierLeft);
 }
 
-// zet alle motor outputs op 0
-void stopMotors() {
-  analogWrite(RIGHT_FORWARD, 0);
-  analogWrite(LEFT_FORWARD, 0);
-  analogWrite(RIGHT_BACKWARD, 0);
-  analogWrite(LEFT_BACKWARD, 0);
-}
-
-// print sensorwaardes voor debug
-void printReadings() {
+void printReadings(){
+  int i = 0;
   Serial.println("---------------");
-  for (int i = 0; i < 8; i++) {
+  for(int reading : sensorReadings){
     Serial.print(i);
     Serial.print(": ");
-    Serial.println(sensorReadings[i]);
+    Serial.println(reading);
+    i++;
   }
 }
