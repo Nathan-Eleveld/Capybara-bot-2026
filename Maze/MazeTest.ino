45d2f49af585dd551fed392de40c3a21c05acca2
AtakanKaracaNHL <atakan.karaca@student.nhlstenden.com>
2026-03-11 14:56:09 +0100

    Refactor maze navigation logic and constants

--- Maze/MazeTest.ino
+++ Maze/MazeTest.ino
@@ -14,332 +14,28 @@ const int LEFT_IN  = 3;
 const int GRIPPER  = 11;
 
 // Afstanden
-const int FRONT_BLOCKED_DISTANCE = 14;
-const int FRONT_OPEN_DISTANCE    = 20;
-const int LEFT_OPEN_DISTANCE     = 22;
-const int MAX_VALID_DISTANCE     = 180;
-const int INVALID_DISTANCE       = 999;
-
-// Sensor timeout
-const unsigned long SONAR_TIMEOUT_US = 25000UL;
+const int LEFT_MIN_TARGET = 12;
+const int LEFT_MAX_TARGET = 15;
+const int FRONT_BLOCKED   = 18;
+const int LEFT_OPENING    = 20;
 
 // Snelheden
-const int DRIVE_SPEED   = 150;
-const int TURN_SPEED    = 165;
-const int REVERSE_SPEED = 130;
+const int BASE_SPEED      = 150;
+const int TURN_SPEED      = 165;
+const int CORRECT_SPEED   = 145;
+const int STRONG_SPEED    = 175;
 
-// Tijden
-const unsigned long LOOP_DELAY_MS        = 40;
-const unsigned long STOP_BEFORE_TURN_MS  = 120;
-const unsigned long STOP_AFTER_TURN_MS   = 80;
-const unsigned long LEFT_TURN_TIME_MS    = 320;
-const unsigned long RIGHT_TURN_TIME_MS   = 320;
-const unsigned long FORWARD_STEP_MS      = 140;
-const unsigned long REVERSE_TIME_MS      = 180;
-const unsigned long DEBUG_INTERVAL_MS    = 250;
-const unsigned long ACTION_COOLDOWN_MS   = 250;
+// Tijden (afhankelijk van chassis aanpassen)
+const int TURN_90_TIME    = 390;
+const int TURN_180_TIME   = 780;
+const int BACKUP_TIME     = 220;
+const int STOP_TIME       = 80;
 
-// Anti-vastloop
-const int MAX_SAME_ACTION_COUNT = 8;
-const int MAX_FLIP_COUNT        = 8;
-const unsigned long FLIP_WINDOW_MS = 2500;
-
-enum Action {
-  ACTION_STOP,
-  ACTION_FORWARD,
-  ACTION_TURN_LEFT,
-  ACTION_TURN_RIGHT,
-  ACTION_REVERSE_STUCK
-};
-
-int frontDistance = INVALID_DISTANCE;
-int leftDistance  = INVALID_DISTANCE;
-
-int lastFrontDistance = 40;
-int lastLeftDistance  = 20;
-
-Action currentAction = ACTION_STOP;
-Action lastAction    = ACTION_STOP;
-
-unsigned long lastDebugTime = 0;
-unsigned long lastActionTime = 0;
-unsigned long flipWindowStart = 0;
-
-int sameActionCount = 0;
-int flipCount = 0;
-bool antiStuckActive = false;
-
-int medianOf3(int a, int b, int c) {
-  if (a > b) { int t = a; a = b; b = t; }
-  if (b > c) { int t = b; b = c; c = t; }
-  if (a > b) { int t = a; a = b; b = t; }
-  return b;
-}
-
-const char* actionToText(Action action) {
-  switch (action) {
-    case ACTION_STOP:          return "STOP";
-    case ACTION_FORWARD:       return "FORWARD";
-    case ACTION_TURN_LEFT:     return "TURN_LEFT";
-    case ACTION_TURN_RIGHT:    return "TURN_RIGHT";
-    case ACTION_REVERSE_STUCK: return "REVERSE_STUCK";
-    default:                   return "UNKNOWN";
-  }
-}
-
-// Motor aansturing
-void applyMotor(int forwardPin, int backwardPin, int speedValue) {
-  speedValue = constrain(speedValue, -255, 255);
-
-  if (speedValue > 0) {
-    analogWrite(forwardPin, speedValue);
-    analogWrite(backwardPin, 0);
-  } else if (speedValue < 0) {
-    analogWrite(forwardPin, 0);
-    analogWrite(backwardPin, -speedValue);
-  } else {
-    analogWrite(forwardPin, 0);
-    analogWrite(backwardPin, 0);
-  }
-}
-
-void setMotorSpeeds(int leftSpeed, int rightSpeed) {
-  applyMotor(LEFT_FORWARD, LEFT_BACKWARD, leftSpeed);
-  applyMotor(RIGHT_FORWARD, RIGHT_BACKWARD, rightSpeed);
-}
-
-void stopMotors() {
-  setMotorSpeeds(0, 0);
-}
-
-void driveForward() {
-  setMotorSpeeds(DRIVE_SPEED, DRIVE_SPEED);
-}
-
-void reverseShort() {
-  setMotorSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
-}
-
-void turnLeftInPlace() {
-  setMotorSpeeds(-TURN_SPEED, TURN_SPEED);
-}
-
-void turnRightInPlace() {
-  setMotorSpeeds(TURN_SPEED, -TURN_SPEED);
-}
-
-// Sensor meting
-int measureDistanceRaw(int trigPin, int echoPin) {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(3);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  unsigned long duration = pulseIn(echoPin, HIGH, SONAR_TIMEOUT_US);
-
-  if (duration == 0) return INVALID_DISTANCE;
-
-  int distance = duration / 58;
-
-  if (distance <= 0 || distance > MAX_VALID_DISTANCE) {
-    return INVALID_DISTANCE;
-  }
-
-  return distance;
-}
-
-int readFilteredDistance(int trigPin, int echoPin, int lastValid) {
-  int d1 = measureDistanceRaw(trigPin, echoPin);
-  delayMicroseconds(700);
-  int d2 = measureDistanceRaw(trigPin, echoPin);
-  delayMicroseconds(700);
-  int d3 = measureDistanceRaw(trigPin, echoPin);
-
-  if (d1 == INVALID_DISTANCE) d1 = lastValid;
-  if (d2 == INVALID_DISTANCE) d2 = lastValid;
-  if (d3 == INVALID_DISTANCE) d3 = lastValid;
-
-  int result = medianOf3(d1, d2, d3);
-
-  if (lastValid != INVALID_DISTANCE && abs(result - lastValid) > 40) {
-    result = (result + lastValid) / 2;
-  }
-
-  return result;
-}
-
-void updateDistances() {
-  frontDistance = readFilteredDistance(
-    ULTRA_SONIC_TRIG_FRONT,
-    ULTRA_SONIC_ECHO_FRONT,
-    lastFrontDistance
-  );
-
-  leftDistance = readFilteredDistance(
-    ULTRA_SONIC_TRIG_LEFT,
-    ULTRA_SONIC_ECHO_LEFT,
-    lastLeftDistance
-  );
-
-  lastFrontDistance = frontDistance;
-  lastLeftDistance  = leftDistance;
-}
-
-bool isFrontBlocked() {
-  return frontDistance <= FRONT_BLOCKED_DISTANCE;
-}
-
-bool isFrontOpen() {
-  return frontDistance >= FRONT_OPEN_DISTANCE;
-}
-
-bool isLeftOpen() {
-  return leftDistance >= LEFT_OPEN_DISTANCE;
-}
+// Sensor timeout
+const unsigned long PULSE_TIMEOUT = 25000UL;
 
 // Debug
-void printDebug() {
-  if (millis() - lastDebugTime < DEBUG_INTERVAL_MS) return;
-  lastDebugTime = millis();
-
-  Serial.print("front distance: ");
-  Serial.print(frontDistance);
-  Serial.print(" cm | left distance: ");
-  Serial.print(leftDistance);
-  Serial.print(" cm | actie: ");
-  Serial.print(actionToText(currentAction));
-  Serial.print(" | anti-vastloop: ");
-  Serial.println(antiStuckActive ? "JA" : "NEE");
-}
-
-// Anti-vastloop tracking
-void registerAction(Action action) {
-  currentAction = action;
-
-  if (action == lastAction) {
-    sameActionCount++;
-  } else {
-    sameActionCount = 0;
-  }
-
-  if (flipWindowStart == 0 || millis() - flipWindowStart > FLIP_WINDOW_MS) {
-    flipWindowStart = millis();
-    flipCount = 0;
-  }
-
-  if (action != lastAction) {
-    flipCount++;
-  }
-
-  lastAction = action;
-  lastActionTime = millis();
-}
-
-bool shouldRunAntiStuck() {
-  if (sameActionCount >= MAX_SAME_ACTION_COUNT) return true;
-  if (flipCount >= MAX_FLIP_COUNT) return true;
-  return false;
-}
-
-void runAntiStuck() {
-  antiStuckActive = true;
-  registerAction(ACTION_REVERSE_STUCK);
-  printDebug();
-
-  stopMotors();
-  delay(100);
-
-  reverseShort();
-  delay(REVERSE_TIME_MS);
-
-  stopMotors();
-  delay(100);
-
-  turnRightInPlace();
-  delay(220);
-
-  stopMotors();
-  delay(100);
-
-  sameActionCount = 0;
-  flipCount = 0;
-  flipWindowStart = millis();
-  antiStuckActive = false;
-}
-
-// Bewegingen
-void doForwardStep() {
-  registerAction(ACTION_FORWARD);
-  driveForward();
-  delay(FORWARD_STEP_MS);
-  stopMotors();
-  delay(40);
-}
-
-void doLeftTurn() {
-  registerAction(ACTION_TURN_LEFT);
-
-  stopMotors();
-  delay(STOP_BEFORE_TURN_MS);
-
-  turnLeftInPlace();
-  delay(LEFT_TURN_TIME_MS);
-
-  stopMotors();
-  delay(STOP_AFTER_TURN_MS);
-
-  driveForward();
-  delay(FORWARD_STEP_MS);
-
-  stopMotors();
-  delay(40);
-}
-
-void doRightTurn() {
-  registerAction(ACTION_TURN_RIGHT);
-
-  stopMotors();
-  delay(STOP_BEFORE_TURN_MS);
-
-  turnRightInPlace();
-  delay(RIGHT_TURN_TIME_MS);
-
-  stopMotors();
-  delay(STOP_AFTER_TURN_MS);
-
-  driveForward();
-  delay(FORWARD_STEP_MS);
-
-  stopMotors();
-  delay(40);
-}
-
-// Beslissing zonder zigzag-correcties
-void decideNextMove() {
-  stopMotors();
-  registerAction(ACTION_STOP);
-  delay(50);
-
-  updateDistances();
-
-  bool leftOpen  = isLeftOpen();
-  bool frontOpen = isFrontOpen();
-  bool frontBlocked = isFrontBlocked();
-
-  // Linkerhandregel
-  if (leftOpen) {
-    doLeftTurn();
-    return;
-  }
-
-  if (frontOpen && !frontBlocked) {
-    doForwardStep();
-    return;
-  }
-
-  doRightTurn();
-}
+unsigned long lastDebug = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -355,37 +51,216 @@ void setup() {
   pinMode(ULTRA_SONIC_TRIG_LEFT, OUTPUT);
   pinMode(ULTRA_SONIC_ECHO_LEFT, INPUT);
 
-  pinMode(RIGHT_IN, INPUT_PULLUP);
-  pinMode(LEFT_IN, INPUT_PULLUP);
-
+  pinMode(RIGHT_IN, INPUT);
+  pinMode(LEFT_IN, INPUT);
   pinMode(GRIPPER, OUTPUT);
+
   digitalWrite(GRIPPER, LOW);
-
   stopMotors();
-  delay(300);
-
-  updateDistances();
-  lastFrontDistance = frontDistance;
-  lastLeftDistance  = leftDistance;
-
-  Serial.println("Maze solver gestart");
+  delay(500);
 }
 
 void loop() {
-  updateDistances();
-  printDebug();
+  int frontDistance = readDistanceCm(ULTRA_SONIC_TRIG_FRONT, ULTRA_SONIC_ECHO_FRONT);
+  int leftDistance  = readDistanceCm(ULTRA_SONIC_TRIG_LEFT, ULTRA_SONIC_ECHO_LEFT);
 
-  if (millis() - lastActionTime >= ACTION_COOLDOWN_MS) {
-    decideNextMove();
+  if (millis() - lastDebug > 200) {
+    Serial.print("Front: ");
+    Serial.print(frontDistance);
+    Serial.print(" cm | Left: ");
+    Serial.print(leftDistance);
+    Serial.println(" cm");
+    lastDebug = millis();
   }
 
-  if (shouldRunAntiStuck()) {
-    runAntiStuck();
+  // 1. Als vooruit vrij is: altijd vooruit rijden
+  if (frontDistance > FRONT_BLOCKED) {
+    followLeftWall(leftDistance);
+    return;
   }
 
-  delay(LOOP_DELAY_MS);
+  // 2. Voor geblokkeerd: eerst links proberen als daar opening is
+  stopBrief();
+
+  if (leftDistance > LEFT_OPENING) {
+    backupShort();
+    turnLeft90();
+    return;
+  }
+
+  // 3. Voor dicht en links dicht:
+  // probeer rechts door een kwartslag rechts te draaien
+  backupShort();
+  turnRight90();
+  delay(60);
+
+  frontDistance = readDistanceCm(ULTRA_SONIC_TRIG_FRONT, ULTRA_SONIC_ECHO_FRONT);
+  if (frontDistance > FRONT_BLOCKED) {
+    return;
+  }
+
+  // 4. Nog steeds vast: nog een kwartslag rechts = 180 vanaf originele richting
+  backupShort();
+  turnRight90();
+  delay(60);
+
+  frontDistance = readDistanceCm(ULTRA_SONIC_TRIG_FRONT, ULTRA_SONIC_ECHO_FRONT);
+  if (frontDistance > FRONT_BLOCKED) {
+    return;
+  }
+
+  // 5. Echt vast: nog stukje achteruit en opnieuw 180 proberen
+  backupLong();
+  turnAround180();
+  delay(60);
 }
+
+int readDistanceCm(int trigPin, int echoPin) {
+  int a = singleReadDistanceCm(trigPin, echoPin);
+  delay(5);
+  int b = singleReadDistanceCm(trigPin, echoPin);
+  delay(5);
+  int c = singleReadDistanceCm(trigPin, echoPin);
+
+  return median3(a, b, c);
+}
+
+int singleReadDistanceCm(int trigPin, int echoPin) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(3);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  unsigned long duration = pulseIn(echoPin, HIGH, PULSE_TIMEOUT);
+
+  // Geen echo = behandel als ver/open
+  if (duration == 0) {
+    return 250;
   }
 
-  delay(LOOP_DELAY_MS);
+  int distance = duration * 0.0343 / 2.0;
+
+  if (distance < 2) distance = 2;
+  if (distance > 250) distance = 250;
+
+  return distance;
+}
+
+int median3(int a, int b, int c) {
+  if ((a >= b && a <= c) || (a >= c && a <= b)) return a;
+  if ((b >= a && b <= c) || (b >= c && b <= a)) return b;
+  return c;
+}
+
+// Houd de linkermuur tussen 12 en 15 cm
+void followLeftWall(int leftDistance) {
+  // Linkermuur kwijt? rustig links zoeken maar wel vooruit
+  if (leftDistance > LEFT_OPENING) {
+    drive(CORRECT_SPEED, STRONG_SPEED);
+    delay(40);
+    return;
+  }
+
+  // Te dicht op de linkermuur -> iets naar rechts sturen
+  if (leftDistance < LEFT_MIN_TARGET) {
+    drive(STRONG_SPEED, CORRECT_SPEED);
+    delay(35);
+    return;
+  }
+
+  // Te ver van de linkermuur -> iets naar links sturen
+  if (leftDistance > LEFT_MAX_TARGET) {
+    drive(CORRECT_SPEED, STRONG_SPEED);
+    delay(35);
+    return;
+  }
+
+  // Mooie afstand -> rechtdoor
+  drive(BASE_SPEED, BASE_SPEED);
+  delay(35);
+}
+
+void drive(int leftSpeed, int rightSpeed) {
+  leftSpeed = constrain(leftSpeed, 0, 255);
+  rightSpeed = constrain(rightSpeed, 0, 255);
+
+  analogWrite(LEFT_FORWARD, leftSpeed);
+  analogWrite(LEFT_BACKWARD, 0);
+
+  analogWrite(RIGHT_FORWARD, rightSpeed);
+  analogWrite(RIGHT_BACKWARD, 0);
+}
+
+void driveBackward(int leftSpeed, int rightSpeed) {
+  leftSpeed = constrain(leftSpeed, 0, 255);
+  rightSpeed = constrain(rightSpeed, 0, 255);
+
+  analogWrite(LEFT_FORWARD, 0);
+  analogWrite(LEFT_BACKWARD, leftSpeed);
+
+  analogWrite(RIGHT_FORWARD, 0);
+  analogWrite(RIGHT_BACKWARD, rightSpeed);
+}
+
+void rotateLeft(int speed) {
+  speed = constrain(speed, 0, 255);
+
+  analogWrite(LEFT_FORWARD, 0);
+  analogWrite(LEFT_BACKWARD, speed);
+
+  analogWrite(RIGHT_FORWARD, speed);
+  analogWrite(RIGHT_BACKWARD, 0);
+}
+
+void rotateRight(int speed) {
+  speed = constrain(speed, 0, 255);
+
+  analogWrite(LEFT_FORWARD, speed);
+  analogWrite(LEFT_BACKWARD, 0);
+
+  analogWrite(RIGHT_FORWARD, 0);
+  analogWrite(RIGHT_BACKWARD, speed);
+}
+
+void stopMotors() {
+  analogWrite(LEFT_FORWARD, 0);
+  analogWrite(LEFT_BACKWARD, 0);
+  analogWrite(RIGHT_FORWARD, 0);
+  analogWrite(RIGHT_BACKWARD, 0);
+}
+
+void stopBrief() {
+  stopMotors();
+  delay(STOP_TIME);
+}
+
+void backupShort() {
+  driveBackward(145, 145);
+  delay(BACKUP_TIME);
+  stopBrief();
+}
+
+void backupLong() {
+  driveBackward(155, 155);
+  delay(BACKUP_TIME + 120);
+  stopBrief();
+}
+
+void turnLeft90() {
+  rotateLeft(TURN_SPEED);
+  delay(TURN_90_TIME);
+  stopBrief();
+}
+
+void turnRight90() {
+  rotateRight(TURN_SPEED);
+  delay(TURN_90_TIME);
+  stopBrief();
+}
+
+void turnAround180() {
+  rotateRight(TURN_SPEED);
+  delay(TURN_180_TIME);
+  stopBrief();
 }
