e28e227d58cc9637f3ff55fb22c8182e2dd6f7d6
AtakanKaracaNHL <atakan.karaca@student.nhlstenden.com>
2026-03-11 13:03:32 +0100

    Fix ultrasonic echo pin and modify speed settings
    Updated ultrasonic echo pin and adjusted speed constants for motor control.

--- Maze/MazeTest.ino
+++ Maze/MazeTest.ino
@@ -5,21 +5,21 @@ const int ULTRA_SONIC_TRIG_FRONT = 8;
 const int ULTRA_SONIC_ECHO_FRONT = 7;
 
 const int ULTRA_SONIC_TRIG_LEFT = 4;
-const int ULTRA_SONIC_ECHO_LEFT = 12;
+const int ULTRA_SONIC_ECHO_LEFT = 13;
 
 const int LEFT_BACKWARD = 6;
 const int LEFT_FORWARD = 5;
 
 const int RIGHT_IN = 2;
 const int LEFT_IN = 3;
-
 const int GRIPPER = 11;
 
 const int WALL_DISTANCE = 20;
+const int SPEED = 180;
 const int TURN_TIME = 450;
-const int FORWARD_SPEED = 180;
 
 void setup() {
+
   pinMode(RIGHT_BACKWARD, OUTPUT);
   pinMode(RIGHT_FORWARD, OUTPUT);
   pinMode(LEFT_BACKWARD, OUTPUT);
@@ -31,30 +31,29 @@ void setup() {
   pinMode(ULTRA_SONIC_TRIG_LEFT, OUTPUT);
   pinMode(ULTRA_SONIC_ECHO_LEFT, INPUT);
 
-  pinMode(RIGHT_IN, INPUT);
-  pinMode(LEFT_IN, INPUT);
-  pinMode(GRIPPER, OUTPUT);
-
   Serial.begin(9600);
 }
 
 void loop() {
+
   int frontDistance = measureDistance(ULTRA_SONIC_TRIG_FRONT, ULTRA_SONIC_ECHO_FRONT);
   int leftDistance = measureDistance(ULTRA_SONIC_TRIG_LEFT, ULTRA_SONIC_ECHO_LEFT);
 
   Serial.print("Front: ");
   Serial.print(frontDistance);
-  Serial.print(" | Left: ");
+  Serial.print("  Left: ");
   Serial.println(leftDistance);
 
   if (leftDistance > WALL_DISTANCE) {
     stopMotors();
     delay(100);
     turnLeft();
-  } 
+  }
+
   else if (frontDistance > WALL_DISTANCE) {
     moveForward();
-  } 
+  }
+
   else {
     stopMotors();
     delay(100);
@@ -63,14 +62,16 @@ void loop() {
 }
 
 int measureDistance(int trigPin, int echoPin) {
+
   digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
+  delayMicroseconds(5);
 
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
+
   digitalWrite(trigPin, LOW);
 
-  long duration = pulseIn(echoPin, HIGH, 30000);
+  long duration = pulseIn(echoPin, HIGH, 40000);
 
   if (duration == 0) {
     return 999;
@@ -80,33 +81,45 @@ int measureDistance(int trigPin, int echoPin) {
 }
 
 void moveForward() {
-  analogWrite(RIGHT_FORWARD, FORWARD_SPEED);
-  analogWrite(LEFT_FORWARD, FORWARD_SPEED);
+
+  analogWrite(RIGHT_FORWARD, SPEED);
+  analogWrite(LEFT_FORWARD, SPEED);
+
   analogWrite(RIGHT_BACKWARD, 0);
   analogWrite(LEFT_BACKWARD, 0);
 }
 
 void stopMotors() {
+
   analogWrite(RIGHT_FORWARD, 0);
   analogWrite(LEFT_FORWARD, 0);
+
   analogWrite(RIGHT_BACKWARD, 0);
   analogWrite(LEFT_BACKWARD, 0);
 }
 
 void turnLeft() {
-  analogWrite(RIGHT_FORWARD, FORWARD_SPEED);
-  analogWrite(LEFT_FORWARD, 0);
+
+  analogWrite(RIGHT_FORWARD, SPEED);
+  analogWrite(LEFT_BACKWARD, SPEED);
+
   analogWrite(RIGHT_BACKWARD, 0);
-  analogWrite(LEFT_BACKWARD, FORWARD_SPEED);
+  analogWrite(LEFT_FORWARD, 0);
+
   delay(TURN_TIME);
+
   stopMotors();
 }
 
 void turnRight() {
-  analogWrite(RIGHT_FORWARD, 0);
-  analogWrite(LEFT_FORWARD, FORWARD_SPEED);
-  analogWrite(RIGHT_BACKWARD, FORWARD_SPEED);
+
+  analogWrite(LEFT_FORWARD, SPEED);
+  analogWrite(RIGHT_BACKWARD, SPEED);
+
   analogWrite(LEFT_BACKWARD, 0);
+  analogWrite(RIGHT_FORWARD, 0);
+
   delay(TURN_TIME);
+
   stopMotors();
 }
