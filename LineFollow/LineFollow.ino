805c1f135475a6cffd5122f4182051b94b3f7399
Friso-klinge <friso.klinge@student.nhlstenden.com>
2026-03-17 14:05:59 +0100

    finished except gripper and sonic sensor

--- LineFollow/LineFollow.ino
+++ LineFollow/LineFollow.ino
@@ -6,15 +6,15 @@
 #define LEFT_IN 3
 #define WHEEL_SPEED 255
 const int   SENSOR_PINS[8] = {A0, A1, A2, A3, A4, A5, A6, A7};
-int   SENSOR_BOUNDRY[8] = {700,700,700,700,700,700,700,700};
-int   sensorReadings[8];
-int   rightPulses = 0;
-int   lastInterruptRight = 0;
-int   leftPulses = 0;
-int   lastInterruptLeft = 0;
-int   rightWheel;
-int   leftWheel;
-int   winFrames;
+int sensorBoundry[8] = {0,0,0,0,0,0,0,0};
+int sensorReadings[8];
+int rightPulses = 0;
+unsigned long lastInterruptRight = 0;
+int leftPulses = 0;
+unsigned long lastInterruptLeft = 0;
+int rightWheel;
+int leftWheel;
+int winFrames;
 
 #define DEBUG
 
@@ -23,9 +23,10 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Battlebot has Started");
   #endif
-
+  
   for(int pin : SENSOR_PINS){
-    pinMode(pin, INPUT);    
+    pinMode(pin, INPUT);   
+    digitalWrite(pin, LOW); 
   }
   pinMode(RIGHT_BACKWARD, OUTPUT);
   pinMode(RIGHT_FORWARD, OUTPUT);
@@ -34,56 +35,46 @@ void setup() {
   pinMode(RIGHT_IN, INPUT);
   pinMode(LEFT_IN, INPUT);
 
+  stopWheels();
+
   attachInterrupt(digitalPinToInterrupt(RIGHT_IN), countPulserightWheelISR, RISING);
   attachInterrupt(digitalPinToInterrupt(LEFT_IN), countPulseleftWheelISR, RISING);
 
-  //calibrateBoundrys();
+  calibrateBoundrys();
   getOnTrack();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  if(/*winDetect()*/ false){
-    stopWheels();
-    } else {
-    #ifdef DEBUG
-    Serial.print(getAverageSensorPin());
-    Serial.print(" | ");
-    Serial.println(winFrames);
-    #endif
-    
-    getReadings();
-    stopWheels();
-    switch(getAverageSensorPin()){
-    case 1:
-      writeWheels(1, 0);
-      break;
-    case 2:
-      writeWheels(1, 0.5);
-      break;
-    case 3:
-      writeWheels(1, 0.8);
-      break;
-    case 4:
-      writeWheels(1, 1);
-      break;
-    case 5:
-      writeWheels(0.8, 1);
-      break;
-    case 6:
-      writeWheels(0.5, 1);
-      break;
-    case 7:
-      writeWheels(0, 1);
-      break;
-    case 0:
-      writeWheels(0, 1);
-      break;
-    }
-    if(!sensorReadings[8]){
-      writeWheels(1, 0);
-    }
+void loop(){
+  getReadings();
+  if(!sensorReadings[1] && (!sensorReadings[5] || !sensorReadings[4])){
+    rightTurn();
+  } else{
+    drive(); 
   }
+}
+
+void calibrateBoundrys(){
+  int temp[8];
+  int readingCount = 0;
+  while(rightPulses < 10){
+    analogWrite(RIGHT_FORWARD, WHEEL_SPEED);
+    analogWrite(LEFT_FORWARD, WHEEL_SPEED);
+    for(int i = 0; i < 8; i++){
+      temp[i] = sensorBoundry[i] + analogRead(SENSOR_PINS[i]);
+      sensorBoundry[i] = temp[i];
+      Serial.println(sensorBoundry[i]);
+    }
+    readingCount++;
+    Serial.println("---");
+  }
+   
+  for(int i = 0; i < 8; i++){
+    sensorBoundry[i] = temp[i] / readingCount;
+    Serial.println(sensorBoundry[i]);
+  }
+  analogWrite(RIGHT_FORWARD, 0);
+  analogWrite(LEFT_FORWARD, 0);
+  delay(200);
 }
 
 //Function called when right wheel rotates 1/20th of a rotation
@@ -102,81 +93,45 @@ void countPulseleftWheelISR(){
   }
 }
 
-void writeWheels(float multiplierRight, float multiplierLeft){
-  analogWrite(RIGHT_FORWARD, WHEEL_SPEED * multiplierRight);
-  analogWrite(LEFT_FORWARD, WHEEL_SPEED * multiplierLeft);
-}
-
-void calibrateBoundrys(){
-  int temp[8];
-  int readingCount = 0;
-  while(millis() < 500){
-    analogWrite(RIGHT_FORWARD, WHEEL_SPEED);
-    analogWrite(LEFT_FORWARD, WHEEL_SPEED);
-    for(int i = 0; i < 8; i++){
-      temp[i] = SENSOR_BOUNDRY[i] + analogRead(SENSOR_PINS[i]);
-      SENSOR_BOUNDRY[i] = temp[i];
-    }
-    readingCount++;
-  }
-   
-  for(int i = 0; i < 8; i++){
-    SENSOR_BOUNDRY[i] = temp[i] / readingCount;
-    #ifdef DEBUG
-    Serial.println(SENSOR_BOUNDRY[i]);
-    Serial.println("---");
-    #endif
-  }
-  analogWrite(RIGHT_FORWARD, 0);
-  analogWrite(LEFT_FORWARD, 0);
-  delay(200);
-}
-
 void getReadings(){
   int i = 0;
   for(int pin : SENSOR_PINS){
-    sensorReadings[i] = (analogRead(pin) < SENSOR_BOUNDRY[i]);
+    sensorReadings[i] = (analogRead(pin) < sensorBoundry[i]);
     i++;
   }
 }
 
-void getOnTrack(){
-  while(millis() < 1000){
-    analogWrite(RIGHT_FORWARD, WHEEL_SPEED);
-    analogWrite(LEFT_FORWARD, WHEEL_SPEED);
+void drive(){
+    stopWheels();
+  
+  switch(getAverageSensorPin()){
+    case 1:
+      writeWheels(1, 0);
+      break;
+    case 2:
+      writeWheels(1, 0.1);
+      break;
+    case 3:
+      writeWheels(1, 0.5);
+      break;
+    case 4:
+      writeWheels(1, 1);
+      break;
+    case 5:
+      writeWheels(0.5, 1);
+      break;
+    case 6:
+      writeWheels(0.1, 1);
+      break;
+    case 7:
+      writeWheels(0, 1);
+      break;
+    case 0:
+      analogWrite(RIGHT_BACKWARD, WHEEL_SPEED * 0.8);
+      analogWrite(LEFT_FORWARD, WHEEL_SPEED * 0.8);
+      Serial.print("turning Left");
+      break;
   }
-  analogWrite(RIGHT_FORWARD, 0);
-  analogWrite(LEFT_FORWARD, 0);
-  leftPulses = 0;
-  rightPulses = 0;
-  while(millis() < 1300){
-    analogWrite(LEFT_FORWARD, WHEEL_SPEED);
-  }
-  analogWrite(LEFT_FORWARD, 0);
-}
-
-bool winDetect(){
-  int sensorsActive = 0;
-  for(int reading : sensorReadings){
-    sensorsActive += reading;
-  }
-  if(sensorsActive < 2){
-    winFrames++;
-  }else{
-    winFrames = 0;
-  }
-  if(winFrames > 20){
-    return true;
-  } else {
-    return false;
-  }
-}
-
-void stopWheels(){
-  analogWrite(RIGHT_FORWARD, 0);
-  analogWrite(LEFT_FORWARD, 0);
-  analogWrite(RIGHT_BACKWARD, 0);
-  analogWrite(LEFT_BACKWARD, 0);
 }
 
 int getAverageSensorPin(){
@@ -190,4 +145,72 @@ int getAverageSensorPin(){
   }  
   
   return medianReading / readingCount;
+}
+
+void writeWheels(float multiplierRight, float multiplierLeft){
+  analogWrite(RIGHT_FORWARD, WHEEL_SPEED * multiplierRight);
+  analogWrite(LEFT_FORWARD, WHEEL_SPEED * multiplierLeft);
+}
+
+void getOnTrack(){
+  stopWheels();
+  while(rightPulses < 10 && leftPulses < 10){
+    analogWrite(RIGHT_FORWARD, WHEEL_SPEED);
+    analogWrite(LEFT_FORWARD, WHEEL_SPEED);
+    Serial.println(rightPulses);
+  }
+  stopWheels();
+  while(rightPulses < 15){
+    analogWrite(LEFT_FORWARD, WHEEL_SPEED);
+    Serial.println(rightPulses);
+  }
+  analogWrite(LEFT_FORWARD, 0);
+}
+
+void stopWheels(){
+  analogWrite(RIGHT_FORWARD, 0);
+  analogWrite(LEFT_FORWARD, 0);
+  analogWrite(RIGHT_BACKWARD, 0);
+  analogWrite(LEFT_BACKWARD, 0);
+  leftPulses = 0;
+  rightPulses = 0;
+}
+
+void rightTurn(){
+  checkForFinish();
+  Serial.print("turning Right");
+  while(leftPulses < 15){
+    analogWrite(RIGHT_FORWARD, WHEEL_SPEED);
+    Serial.println(leftPulses);
+    Serial.print("leftpulses: ");
+  }
+  stopWheels();
+}
+
+void checkForFinish(){
+  stopWheels();
+  while(leftPulses < 5){
+    analogWrite(RIGHT_FORWARD, WHEEL_SPEED);
+    analogWrite(LEFT_FORWARD, WHEEL_SPEED);
+  }
+  stopWheels();
+  delay(50);
+  if(mostlyBlack()){
+    while(true){}
+  }
+  while(leftPulses < 6){
+    analogWrite(RIGHT_BACKWARD, WHEEL_SPEED);
+    analogWrite(LEFT_BACKWARD, WHEEL_SPEED);
+  }
+  stopWheels();
+  delay(50);
+}
+
+boolean mostlyBlack(){
+  getReadings();
+  int count = 0;
+  for(int reading : sensorReadings){
+    count += reading;
+  }
+  return (count < 6);
 }
